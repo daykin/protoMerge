53bca0a31166485d32c0444b5d3d16f68108dee7
Daykin, Evan <daykin@frib.msu.edu>
2015-08-11 13:25:52 -0400

    threading and hash check

--- FileCompare/protoBufMerger.py
+++ FileCompare/protoBufMerger.py
@@ -6,34 +6,37 @@ import sys
 import os
 from os.path import join
 import time
+from collections import OrderedDict
 
-def getFileHash(file):
+def getFileHash(file): #return the hash of an entire file.
     m = hashlib.md5()
     for line in file:
         m.update(line.strip())
     file.seek(0)    
     return m.hexdigest()
 
-def mergeWorkerThread(folder):
-    hashes = {}
-    seenLines = {}
+def mergeWorkerThread(folder): #thread to merge all PBs in a given folder
+    hashes = {} 
+    seen = {}
     result = []
     dupl = 0
     lineCount = 0
     begin = time.clock()
-    logging.info("[{:s}]Starting merge for {:s}.".format(time.asctime(), folder)) 
-    with open(join(folder,"{:s}_merged.PB".format(folder)),"wb") as merged:
+    logging.info("[{:s}] Starting merge for {:s}.".format(time.asctime(), folder)) 
+    with open(join(folder,"{:s}_merged.PB".format(folder)),"wb") as merged:  #open an output file
         for file in gl.glob(join(folder,"*.pb")):
             with open(file, "rb") as f:
-                hash = getFileHash(f)
-                if hash in hashes:
+                hash = getFileHash(f) 
+                if hash in hashes: #see if we already looked at an exact duplicate.
                     logging.info("{:s} is an exact duplicate of {:s}. skipping line-by-line checks and removing...".format(file, hashes.get(hash)))
-                    dupl += len(f.readlines())
+                    lncount= len(f.readlines())
+                    dupl+= duplncount
+                    lineCount += duplncount
                     f.close()
                     os.remove(file)
                     continue
-                hashes[hash] = file
-                for line in f:
+                hashes[hash] = file  #if not, record the unique
+                for line in f:       #check line-by-line. add uniques to a dict
                     lineCount += 1
                     if line in seen: 
                         dupl+=1
@@ -41,7 +44,7 @@ def mergeWorkerThread(folder):
                     seen[line] = 1
                     result.append(line)                    
         merged.writelines(result)
-    logging.info("[{:s}] merged files for {:s}.operation took {:f} seconds. {:d} records with {:d} duplicate(s).".format(time.asctime(),folder,time.clock()-begin,lineCount,dupl))
+    logging.info("[{:s}] Merged files for {:s}. Operation took {:f} seconds. {:d} records processed with {:d} duplicate(s).".format(time.asctime(),folder,time.clock()-begin,lineCount,dupl))
     threads.remove(threading.currentThread().getName())
         
 def mergeScheduler(maxThreads=5):       
@@ -54,8 +57,9 @@ def mergeScheduler(maxThreads=5):
             t.start()
                        
 logging.basicConfig(filename = "merge_{:d}.log".format(int(time.time())),level = logging.INFO)
+
 threads = []
 folders = []
 mergeScheduler()
-sys.exit()
+
 
