266b927ff1437c55cf4cf9ca95a98df249f0b95e
Daykin, Evan <daykin@frib.msu.edu>
2015-08-12 16:57:15 -0400

    modifications for archiver directory structure

--- FileCompare/protoBufMerger.py
+++ FileCompare/protoBufMerger.py
@@ -1,13 +1,12 @@
 ﻿import logging
 import hashlib
 import threading
-import glob as gl
 import sys
-from pandas import DataFrame
 import os
 from os.path import join
 import time
 import EPICSEvent_pb2 as pb
+import argparse
 
 def getFileHash(file):                  #return the hash of an entire file.
     m = hashlib.md5()
@@ -16,65 +15,63 @@ def getFileHash(file):                  #return the hash of an entire file.
     file.seek(0)    
     return m.hexdigest()
 
-def mergeWorkerThread(folder):          #thread to merge all PBs in a given folder
-    hashes = {} 
+def mergeOperationThread(files, mergeFile):    
     seen = {}
     result = []
     dupl = 0
     lineCount = 0
-    thisVal = None
-    lastVal = None
-    begin = time.clock()
-    logging.info("[{:s}] Starting merge for {:s}.".format(time.asctime(), folder)) 
-    with open(join(folder,"{:s}".format(folder)),"wb") as merged:  #open an output file
-        for file in gl.glob(join(folder,"*.pb")):
-            with open(file, "rb") as f:
-                hash = getFileHash(f) 
+    for f in files:
+        try:
+            with open(f,"rb") as input:
+                inHash = getFileHash(input)
                 if hash in hashes:      #see if we already looked at an exact duplicate.
-                    logging.info("[{:s}] {:s} is an exact duplicate of {:s}. removing...".format(time.asctime(),file, hashes.get(hash)))
+                    logging.info("[{:s}] {:s} is an exact duplicate of {:s}. skipping line-by-line checks and removing...".format(time.asctime(),file, hashes.get(hash)))
                     lncount= len(f.readlines())
                     dupl+= duplncount
                     lineCount += duplncount
                     f.close()
-                    #os.remove(file)
+                    os.remove(file)
                     continue
-                hashes[hash] = file     #if not, record the unique hash
-                result.append(f.readline()) # add the header
-                for line in f:          #check line-by-line. add uniques to a dict for speedy lookup
+                fileHashes.add(inHash)
+                for line in input:      #check line-by-line. add uniques to a dict for speedy lookup
                     lineCount += 1
-                    if line in seen:    #parsing is slow, avoid it if we can (exact duplicate line)
-                        dupl += 1
+                    if line in seen: 
+                        dupl+=1
                         continue
                     seen[line] = 1      #add entry to dict
-                    dbl = pb.ScalarDouble()   #pure python, runs abysmally slow
-                    try:
-                        dbl.parsefromstring(line.strip())
-                    except:
-                        logging.info("truncated message")
-                        pass
-                    thisval = dbl.val
-                    if thisval == lastval: #skip unchanged values
-                        dupl += 1
-                        continue
-                    lastval = thisval
-                    result.append(line) #add the unique to a list to retain order    
-        print "beginning write."               
-        merged.writelines(result)
-    logging.info("[{:s}] Merged files for {:s}. Operation took {:f} seconds. {:d} records processed with {:d} duplicate(s).".format(time.asctime(),folder,time.clock()-begin,lineCount,dupl))
-    threads.remove(threading.currentThread().getName())
- 
-def mergeScheduler(maxThreads=5):       
-    begin = time.clock()
-    for folder in gl.glob("20*"):
-        folders.append(folder)
-    while folders:        
-        if (len(threads) < maxThreads):
-            t = threading.Thread(target=mergeWorkerThread,args=(folders.pop(),))
+                    result.append(line) #add the unique to a list to retain order
+        except:
+             logging.warn("no corresponding file in opposite folder.")
+             
+             continue 
+    threads.remove(threading.currentThread().getName())      
+
+def mergeOperationScheduler(subfolder1, subfolder2, maxThreads):          #thread to merge all PBs in a given folder
+    files1 = [file for file in os.listdir(subfolder1)]
+    files2 = [file for file in os.listdir(subfolder2)]
+    files1.extend(files2)
+    files = set(files1)
+    while files:
+        if (len(threads) < maxThreads): 
+            t = threading.Thread(target=mergeOperationThread, args = ([join(subfolder1,file),join(subfolder2,file)]))  #todo: don't reinvent wheel, use Queue
             threads.append(t.getName())
             t.start()
-    logging.info("main thread done in {:f} seconds.".format(time.clock()-begin))
+
+ 
+def mergeMaster(folder1, folder2, maxThreads=5):       
+    subdirs = [subdir for subdir in os.listdir(folder1)]
+    for subdir in subdirs:
+        mergeOperationScheduler(join(folder1,subdir),join(folder2,subdir),)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("maxThreads", help = "maximum allowed parallel threads", type = int)
+parser.add_argument("folder1", help = "first merge directory")
+parser.add_argument("folder2", help = "second merge directory")
+parser.add_argument("mergeFolder", help = "where to place the output")
+args = parser.parse_args()
                        
 logging.basicConfig(filename = "merge_{:d}.log".format(int(time.time())),level = logging.INFO)
+fileHashes = set()
 threads = []
-folders = []
-mergeScheduler()
+mergeScheduler("app1","app2", "merge",5)
