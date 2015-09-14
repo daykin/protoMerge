1be1a4082b5600addd1bd36e87090addbfed8f3d
Evan Daykin <daykin@frib.msu.edu>
2015-09-14 12:47:43 -0700

    hash check shows different files as equal

--- FileCompare/protoBufMerger.py
+++ FileCompare/protoBufMerger.py
@@ -7,11 +7,12 @@ from os.path import join
 import time
 import argparse
 
-def getFileHash(file):                  #return the hash of an entire file.
+def getFileHash(f):                  #return the hash of an entire file.
     m = hashlib.md5()
-    for line in file:
+    for line in f:
         m.update(line.strip())
-    file.seek(0)    
+    f.seek(0)
+    print(m.hexdigest())
     return m.hexdigest()
 
 def mergeOperationThread(files, mergeFile):    
@@ -20,51 +21,57 @@ def mergeOperationThread(files, mergeFile):
     dupl = 0
     lineCount = 0
     begin = time.clock()
-    for f in files:
-        with open(f,"rb") as input, open(mergeFile,args.mergemode+"+b") as merge:
-            inHash = getFileHash(input)
-            if inHash in fileHashes:      #see if we already looked at an exact duplicate.
-                logging.info("[{:s}] {:s} is an exact duplicate of {:s}. skipping line-by-line checks and removing...".format(time.asctime(),file, hashes.get(hash)))
-                lncount= len(f.readlines())
-                dupl+= lncount
-                f.close()
-                os.remove(file)
-                continue
-            fileHashes.add(inHash)
-            for line in input:      #check line-by-line. add uniques to a dict for speedy lookup
-                lineCount += 1
-                if line in seen: 
-                    dupl+=1
+    with open(mergeFile,args.mergemode+"+b") as merge:
+        for f in files:
+            with open(f,"rb") as infile:
+                inHash = getFileHash(infile)
+                if inHash in fileHashes:      #see if we already looked at an exact duplicate.
+                    logging.info("[{:s}] {:s} is an exact duplicate of {:s}. skipping line-by-line checks ..."\
+                                 .format(time.asctime(),f, fileHashes.get(inHash)))
+                    lncount= len(infile.readlines())
+                    dupl+= lncount
+                    infile.close()
+                    #os.remove(file)
                     continue
-                seen[line] = 1      #add entry to dict
-                result.append(line) #add the unique to a list to retain order              
-            merge.writelines(result)
+                fileHashes[inHash] = f
+                if os.stat(mergeFile).st_size != 0:  
+                    infile.readline()    #throw out the header if the merge file already has something
+                for line in infile:      #check line-by-line. add uniques to a dict for speedy lookup
+                    lineCount += 1
+                    if line in seen: 
+                        dupl+=1
+                        continue
+                    seen[line] = 1      #add entry to dict
+                    result.append(line) #add the unique to a list to retain order              
+                merge.writelines(result)
     logging.info("[{:s}] completed merge for {:s} in {:f} seconds. found {:d} duplicate lines.".format(time.asctime(),f,time.clock()-begin,dupl))
     threads.remove(threading.currentThread().getName())      
 
 def mergeOperationScheduler(subfolder1, subfolder2, maxThreads=5):          #thread to merge all PBs in a given folder
-    files1 = set(file for file in os.listdir(subfolder1))
-    files2 = set(file for file in os.listdir(subfolder2))
+    files1 = set(filename for filename in os.listdir(subfolder1))
+    files2 = set(filename for filename in os.listdir(subfolder2))
     basename = os.path.basename(subfolder1)                                 #Master already made sure this exists in both subfolders 
+    #basename2 = os.path.basename(subfolder2)
     toMerge = files1.intersection(files2)
     singles = files1.union(files2).difference(toMerge)
     if singles:
-       shutil.copy((file for file in singles),join(args.mergeFolder,basename))
-       logging.info("one or more files in {:s} was not mirrored. file(s) copied to merge directory.")             
+        for filename in singles:
+            shutil.copy(filename,join(args.mergefolder,basename))
+            logging.info("{:s} is not mirrored. file copied to merge directory.".format(filename))             
     while toMerge:
         file = toMerge.pop()
         mergedir = join(args.mergefolder,basename)
         if not os.path.exists(mergedir):
+            logging.info("[{:s}]supplied merge path doesn't exist. creating it.".format(time.asctime))
             os.makedirs(mergedir)           
         if (len(threads) < maxThreads): 
             t = threading.Thread(target=mergeOperationThread, args = ([join(subfolder1,file),join(subfolder2,file)],join(mergedir,file)))  
             threads.append(t.getName())
             t.start()
 
- 
-def mergeMaster(folder1, folder2):        
-    subdirs1 = set(subdir for subdir in os.listdir(folder1))
-    subdirs2 = set(subdir for subdir in os.listdir(folder2))
+def mergeMaster(folder1 = os.path.join(os.getcwd(),"appl0"), folder2 = os.path.join(os.getcwd(),"appl1")):        
+    subdirs1 = set(os.path.join(folder1,subdir) for subdir in os.listdir(folder1))
+    subdirs2 = set(os.path.join(folder1,subdir) for subdir in os.listdir(folder2))
     common = subdirs1.intersection(subdirs2)                    
     singles = subdirs1.union(subdirs2).difference(common)   #subdirs that exist in 1 xor 2
     for subdir in singles:                                  #take care of the singles
@@ -73,7 +80,6 @@ def mergeMaster(folder1, folder2):
             shutil.copy(subdir, args.mergefolder)
         except Exception, e:
             logging.warning(e)
-            pass
     for subdir in common:                                   #merge the mirrored files
         mergeOperationScheduler(join(folder1,subdir),join(folder2,subdir),args.maxthreads)
 
@@ -87,9 +93,9 @@ args = parser.parse_args()
                        
 logging.basicConfig(filename = "merge_{:d}.log".format(int(time.time())),level = logging.INFO)
 
-if(args.mergemode == "w" or args.mergemode == "a"):
-    fileHashes = set()
-    threads = []
-    mergeMaster(args.folder1,args.folder2)
-else:
-    print "invalid mode for merge file."
+if not(args.mergemode == "w" or args.mergemode == "a"):
+    args.mergemode = "w"
+fileHashes = {}
+threads = []
+mergeMaster(args.folder1,args.folder2)
+
